a4efc8da8463b353d8165cd239c5bc9ecb5b5869
Jan Galinski <jan.galinski@holisticon.de>
2017-09-08 09:58:02 +0200

    remove yaml parse in sb app

--- example/spring-boot/src/main/java/org/camunda/bpm/swagger/example/springboot/MainApplication.java
+++ example/spring-boot/src/main/java/org/camunda/bpm/swagger/example/springboot/MainApplication.java
@@ -3,20 +3,13 @@ package org.camunda.bpm.swagger.example.springboot;
 import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.event.EventListener;
-import org.yaml.snakeyaml.Yaml;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.net.URL;
-import java.util.Map;
 
 @SpringBootApplication
 @EnableProcessApplication
-public class MainApplication implements CommandLineRunner{
+public class MainApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MainApplication.class, args);
@@ -34,12 +27,4 @@ public class MainApplication implements CommandLineRunner{
 
   }
 
-  @Override
-  public void run(String... strings) throws Exception {
-    Yaml yaml = new Yaml();
-
-    Object load = yaml.load(MainApplication.class.getResourceAsStream("camunda-rest-docs.yaml"));
-
-    System.out.println(load);
-  }
 }
