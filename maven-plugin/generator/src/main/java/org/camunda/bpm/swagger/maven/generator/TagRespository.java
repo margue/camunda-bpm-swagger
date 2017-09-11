711b97b6c897c0a2dc1b965310236bc016633e4a
Simon Zambrovski <simon.zambrovski@holisticon.de>
2017-09-11 10:57:08 +0200

    Added missing tag repository

---
+++ maven-plugin/generator/src/main/java/org/camunda/bpm/swagger/maven/generator/TagRespository.java
@@ -0,0 +1,43 @@
+package org.camunda.bpm.swagger.maven.generator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.camunda.bpm.engine.rest.CaseDefinitionRestService;
+import org.camunda.bpm.engine.rest.CaseExecutionRestService;
+import org.camunda.bpm.engine.rest.CaseInstanceRestService;
+import org.camunda.bpm.engine.rest.DecisionDefinitionRestService;
+import org.camunda.bpm.engine.rest.DecisionRequirementsDefinitionRestService;
+import org.camunda.bpm.engine.rest.JobDefinitionRestService;
+import org.camunda.bpm.engine.rest.JobRestService;
+import org.camunda.bpm.engine.rest.ProcessDefinitionRestService;
+import org.camunda.bpm.engine.rest.ProcessInstanceRestService;
+import org.camunda.bpm.swagger.maven.model.CamundaRestService;
+
+public class TagRespository {
+
+  private static Map<Class<?>, String> tags = new HashMap<>();
+  static {
+    tags.put(CaseDefinitionRestService.class, "Case Definition");
+    tags.put(CaseExecutionRestService.class, "Case Execution");
+    tags.put(CaseInstanceRestService.class, "Case Instance");
+
+    tags.put(DecisionDefinitionRestService.class, "Decision Definition");
+    tags.put(DecisionRequirementsDefinitionRestService.class, "Decision Requirement Definition Execution");
+
+    tags.put(JobDefinitionRestService.class, "Job Definition");
+    tags.put(JobRestService.class, "Job Execution");
+
+    tags.put(ProcessDefinitionRestService.class, "Process Definition");
+    tags.put(ProcessInstanceRestService.class, "Process Instance");
+  }
+
+  public static String lookup(final CamundaRestService camundaRestService) {
+    return tags.getOrDefault(camundaRestService.getServiceInterfaceClass(), getTag(camundaRestService));
+  }
+
+  public static String getTag(final CamundaRestService camundaRestService) {
+    return StringHelper.splitCamelCase(camundaRestService.getSimpleName()).split(" ")[0];
+  }
+
+}
